dc919f22035bfa6c79d5d21fb5cff630b870ea35
Devilie <devilie1989@gmail.com>
2017-01-24 12:14:55 +0200

    rewrite modules for work with promise

--- src/modules/utils.js
+++ src/modules/utils.js
@@ -1,35 +1,43 @@
 'use strict';
 
 module.exports = {
-    isActive: function (person) {
-        if (person.isActive) {
-            return true;
-        } else {
-            return false;
-        }
-    },
-    sortAge: function (person1, person2) {
-        if (person1.age > person2.age) {
-            return true;
-        } else {
-            return false;
-        }
-    },
-    sortName: function (person1, person2) {
-        var name1 = person1.name.first + person1.name.last;
-        var name2 = person2.name.first + person2.name.last;
-        if (name1 < name2) {
-            return false;
-        } else {
-            return true;
-        }
-    },
-    overSixCharSurname: function (person) {
-        var surname = person.name.last;
-        if (surname.length >= 6) {
-            return true;
-        } else {
-            return false;
-        }
+    isActive: isActiveUser,
+    sortAge: sortByAge,
+    sortName: sortByName,
+    overSixCharSurname: filterLastNameLongerThanSix
+};
+
+function isActiveUser(person) {
+    if (person.isActive) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+function sortByAge(person1, person2) {
+    if (person1.age > person2.age) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+function sortByName(person1, person2) {
+    var name1 = person1.name.first + person1.name.last;
+    var name2 = person2.name.first + person2.name.last;
+    if (name1 < name2) {
+        return false;
+    } else {
+        return true;
+    }
+};
+
+function filterLastNameLongerThanSix(person) {
+    var surname = person.name.last;
+    if (surname.length >= 6) {
+        return true;
+    } else {
+        return false;
     }
 };
